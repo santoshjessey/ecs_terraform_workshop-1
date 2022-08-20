d63728d27b81ff5f7d99ce110529162b112203ca
santoshjessey <107042702+santoshjessey@users.noreply.github.com>
2022-08-20 18:11:49 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source         = "terraform-aws-modules/vpc/aws"
-  version        = "3.14.2"
+  version        = "2.38.0"
   name           = "test_ecs_provisioning"
   cidr           = "10.0.0.0/16"
   azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
